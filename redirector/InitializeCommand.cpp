1711125ccd5e6127b6d8d9e20d353788a5e2fb79
zhdanov-aa <zhdanov_aa@mail.ru>
2025-05-26 13:48:07 +0400

    Добавлена точка входа main() и команда инициализации

---
+++ redirector/InitializeCommand.cpp
@@ -0,0 +1,11 @@
+#include "InitializeCommand.h"
+#include "IoC.h"
+
+InitializeCommand::InitializeCommand()
+{
+}
+
+void InitializeCommand::Execute()
+{
+    // TODO: Создание зависимостей в IoC
+}
